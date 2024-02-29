93e63b6927929e8d8b7a314a3098b3391f17e764
FW <felix.wenk@pmk.de>
2024-02-29 09:03:11 +0100

    test: removed legacy power supply tests

--- tests/test_power_supplies.py
+++ tests/test_power_supplies.py
@@ -1,6 +1,5 @@
 import pytest
 
-from pmk_probes._hardware_interfaces import USBInterface, LANInterface
 from pmk_probes.power_supplies import PS03, find_power_supplies
 from tests.config import *
 
@@ -13,28 +12,12 @@ def ps():
 
 
 class TestPMKPowerSupply:
-    def test_init_with_usb(self, ps):
-        assert isinstance(ps.interface, USBInterface)
-
-    def test_init_with_lan(self, ps):
-        assert isinstance(ps.interface, LANInterface)
-
-    def test_init_with_no_preferred_interface(self, ps):
-        assert isinstance(ps.interface, USBInterface)
-
-    def test_ps02_num_channels(self, ps):
-        assert ps._num_channels == 2
-
-    def test_ps03_num_channels(self, ps):
-        assert ps._num_channels == 4
+    def test_num_channels(self, ps):
+        assert ps._num_channels in [2, 4]
 
     def test_close(self, ps):
         ps.close()
         assert ps.interface.is_open == False
-
-    def test_connected_probes(self, ps):
-        print(ps.connected_probes)
-        assert 1 == 0
 
 
 def test_find_power_supplies():
